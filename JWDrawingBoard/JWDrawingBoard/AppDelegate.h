5c7ed07cfac924f8347725cb436a893a0fbd7031
sungys <sirius@JJMac.local>
2015-07-01 08:26:48 +0800

    first commit

---
+++ JWDrawingBoard/JWDrawingBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JWDrawingBoard
+//
+//  Created by cuizm on 15/6/30.
+//  Copyright (c) 2015年 sunjws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JWDrawingBoard/JWDrawingBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JWDrawingBoard
+//
+//  Created by cuizm on 15/6/30.
+//  Copyright (c) 2015年 sunjws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
